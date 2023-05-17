40b318549358379bf18d08ddfac5611568db1418
Iman Behroozifar <105700046+imanbehroozifar@users.noreply.github.com>
2023-05-17 17:59:58 +0430

    Send todo to backend

--- components/templates/AddTodoPage/AddTodoPage.js
+++ components/templates/AddTodoPage/AddTodoPage.js
@@ -2,14 +2,31 @@ import { GrAddCircle } from 'react-icons/gr'
 import { BsAlignStart } from 'react-icons/bs'
 import { FiSettings } from 'react-icons/fi'
 import { AiOutlineFileSearch } from 'react-icons/ai'
-import {MdDoneAll} from 'react-icons/md'
+import { MdDoneAll } from 'react-icons/md'
 import { useState } from 'react'
 import RadioBtn from '@/components/elements/RadioBtn'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddTodoPage = () => {
     const [title, setTitle] = useState("")
     const [status, setStatus] = useState("todo")
-    console.log(status)
+    const addHandler = async () => {
+        const res = await fetch("/api/todos",
+            {
+                method: "POST",
+                body: JSON.stringify({ title, status }),
+                headers: { "Content-Type": "application/json" }
+            }
+        )
+        const data = await res.json()
+        console.log(data)
+        if (data.status === "success") {
+            setTitle("")
+            setStatus("todo")
+            toast.success("Todo Added")
+        }
+    }
     return (
         <div className='add-form'>
             <h2><GrAddCircle /> Add New Todo</h2>
@@ -47,10 +64,11 @@ const AddTodoPage = () => {
                         title='Done'>
                         <MdDoneAll />
                     </RadioBtn>
-                   
+
                 </div>
-                <button>Add</button>
+                <button onClick={addHandler}>Add</button>
             </div>
+            <ToastContainer />
         </div>
     )
 }
--- pages/api/todos.js
+++ pages/api/todos.js
@@ -5,18 +5,19 @@ import { getSession } from "next-auth/react";
 export default async function handler(req, res) {
     try {
         await connectDB()
+        console.log("connecting")
     } catch (error) {
         console.log(error.message)
         res.status(500).json({ status: "failed", message: "Error: in Connencting to DB" })
     }
-    const session = getSession({ req })
+    const session = await getSession({ req })
     if (!session) {
         return res.status(401)
             .json({ status: "failed", message: "Error: you not logged in!" })
 
     }
-    const user = User.findOne({ email: session.user.email })
-    if (user) {
+    const user = await User.findOne({ email: session.user.email })
+    if (!user) {
         return res.status(404)
             .json({ status: "failed", message: "Error: user dosent exist" })
     }
@@ -26,7 +27,7 @@ export default async function handler(req, res) {
             return res.status(422)
                 .json({ status: "failed", message: "Error: invalid data" })
         }
-        user.countDocuments.push({ title, status })
+        user.todos.push({ title, status })
         user.save()
         return res.status(201)
             .json({ status: "success", message: "Success: todo Created" })
