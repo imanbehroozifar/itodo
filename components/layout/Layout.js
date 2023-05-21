9bf2dc4cac00bfd63498ddeb7b4f3ff3748ff9fc
Iman Behroozifar <105700046+imanbehroozifar@users.noreply.github.com>
2023-05-21 10:04:31 +0430

    Create a button to log out and restrict access to todos

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,27 +2,40 @@ import Link from "next/link"
 import { VscListSelection } from "react-icons/vsc"
 import { BiMessageSquareAdd } from 'react-icons/bi'
 import { RxDashboard } from 'react-icons/rx'
+import { FiLogOut } from "react-icons/fi"
+import { signOut, useSession } from "next-auth/react"
 
 const Layout = ({ children }) => {
+    const { status } = useSession()
+    const logOutHandler = () => {
+        signOut()
+    }
     return (
         <div className="container">
             <header>
                 <p>ITodo App</p>
+                {
+                    status === "authenticated" ? (
+                        <button onClick={logOutHandler}>Logou <FiLogOut /></button>
+                    ) : null
+                }
+
+
             </header>
             <div className="container--main">
                 <aside>
                     <p>Welcome</p>
                     <ul>
                         <li>
-                            <VscListSelection/>
+                            <VscListSelection />
                             <Link href='/'>Todos</Link>
                         </li>
                         <li>
-                            <BiMessageSquareAdd/>
+                            <BiMessageSquareAdd />
                             <Link href='/add-todo'>Add Todo</Link>
                         </li>
                         <li>
-                            <RxDashboard/>
+                            <RxDashboard />
                             <Link href='/profile'>Profile</Link>
                         </li>
                     </ul>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import HomaePage from "@/components/templates/HomaePage/HomaePage";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
   return (
     <HomaePage/>
   )
+}
+export async function getServerSideProps({ req }) {
+  const session = await getSession({req})
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent:false
+       }
+     }
+  }
+  return {
+    props:{session}
+  }
 }
